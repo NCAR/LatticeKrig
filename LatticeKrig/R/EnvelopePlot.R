2c3019c553b9047981f32b06e4b0edad09322b5a
Matthew Iverson <miverson@mymail.mines.edu>
2019-07-16 12:33:41 -0600

    Final feedback through page 16

--- LatticeKrig/R/EnvelopePlot.R
+++ LatticeKrig/R/EnvelopePlot.R
@@ -1,9 +1,12 @@
-EnvelopePlot <- function(x1, y1, x2 = x1, y2, col = NA, ...) {
+EnvelopePlot <- function(x1, y1, x2 = x1, y2, col = NA, lineCol = NA, ...) {
   if(is.na(col)) {
     col = "thistle1"
   }
+  if(is.na(lineCol)) {
+    lineCol = "thistle3"
+  }
   
   polygon(c(x1, rev(x2)), c(y1, rev(y2)), col = col, border = NA, ...)
-  lines(x1, y1, lwd = 3, col = "thistle3")
-  lines(x2, y2, lwd = 3, col = "thistle3")
+  lines(x1, y1, lwd = 3, col = lineCol)
+  lines(x2, y2, lwd = 3, col = lineCol)
 }
